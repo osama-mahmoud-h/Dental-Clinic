f95e21b59934cde06b5b9836a43a5c2126a32ae7
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2021-08-27 21:14:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/**start loading animation */
+var screen=document.querySelector(".loading");
+  screen.classList.remove("hide");
+  setTimeout(()=>{
+    screen.classList.add("hide");
+  },1500);
+/**end loading animation */
+
 /*close and open toggle menu in mobile*/
 var closeBtn=document.querySelector(".navbar .links ul .close");
 var menuBtn=document.querySelector(".navbar .toggle-btn i");
@@ -5,23 +13,43 @@ var linksList=document.querySelector(".navbar .links ");
 closeBtn.onclick=function(){
     linksList.classList.add('hide');
     linksList.classList.remove('show');
-    console.log("pressed");
 }
 menuBtn.onclick=function(){
     linksList.classList.add('show');
     linksList.classList.remove('hide');
 }
 
+
 /* satrt set link active */
-var link=document.querySelectorAll(".navbar .links ul li a");
+var serv =document.getElementById("services");
+
 link.forEach((ele)=>{
     ele.onclick=function(){
+     //offset top of section
+     var sectionOffsetTop= document.getElementById(ele.getAttribute('data-target').toString()).offsetTop;
+     var body =document.querySelector("html ,body");
+
+     //remove menu in mobile screen
+     linksList.classList.add('hide');
+    linksList.classList.remove('show');
+
+     //smooth scroolling
+   var scrolling = setInterval(()=>{
+       if (body.scrollTop>=sectionOffsetTop-10){
+         clearInterval(scrolling);
+       }else{
+        body.scrollTop+=10;
+       }
+     },3);
+   
+     //remove class active from all links
         link.forEach((sbilings)=>{
             sbilings.classList.remove("active");
         });
+        //add class active to current link
         this.classList.add("active");
-    } 
-});
+    }; 
+})
 /**end set link active */
 
 /**start auto slider */
@@ -43,26 +71,33 @@ var slideArr=Array.from(slides);
 
      if(curSlide==0)
      clearInterval(interval);
-    },3000);
-
-/*bullts[0].onclick=function(){
-  console.log("zzzzzzz");
-}
-document.addEventListener("click",(e)=>{
-console.log(e.target.tagName);
-});
-    bullts.forEach((bullt,index)=>{
-      bullt.onclick=function(){
-        console.log("pressesddddd");
-        curSlide=index;
-        bullts.forEach((ele)=>{
-          ele.classList.remove('active');
-        });
-        slides.forEach((ele)=>{
-          ele.classList.remove('active');
-        });
-        bulltsArr[curSlide].classList.add('active');
-        slideArr[curSlide].classList.add('active');
-      }
-    });*/
+    },2500);
 /**end auto slider */
+
+//**start up button */
+var upButton=document.querySelector(".up-btn");
+
+window.onscroll=function(){
+  var bodyOffsetTop=document.querySelector("html ,body");
+  if(bodyOffsetTop.scrollTop>=800){
+    upButton.classList.remove("hide");
+  }
+  else
+  upButton.classList.add("hide");
+  //on click 
+  upButton.onclick=function(){
+    console.log("preseeee");
+    var scrollingUp=setInterval(() => {
+      if(bodyOffsetTop.scrollTop<=20){
+        clearInterval(scrollingUp);
+      }
+      else{
+        bodyOffsetTop.scrollTop-=20;
+      }
+    }, 1);
+  }
+
+}
+
+/**end up button */
+
